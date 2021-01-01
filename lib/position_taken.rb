41ccd0a9417bec98cb1946429db46a0c01d8f55b
Tanesha Goodman <t24good@icloud.com>
2021-01-01 17:21:26 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,12 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
- if board[position] == ' ' || board[position] == ' ' || board[position] == nil
-   false
-  else
-    true
-  end
+ 
 end
+
+
+##if board[position] == ' ' || board[position] == ' ' || board[position] == nil
+  ##false
+ ##else
+   ##true
+ ##end
+ 
